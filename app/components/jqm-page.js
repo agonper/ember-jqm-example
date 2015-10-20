92670819cbf6c77fc2dde6e7b503afbd0a1ea4e9
albertogonper <alberto.gonzalez95@gmail.com>
2015-10-20 10:51:11 +0200

    jqm-page init event fixed (multitrigger)

--- app/components/jqm-page.js
+++ app/components/jqm-page.js
@@ -6,8 +6,8 @@ export default JqmComponent.extend({
   role: 'page',
   didInsertElement() {
     // This makes first load faster and good rendered
-    $( document ).trigger('jqm-init-page');
-    $( document ).on('jqm-init-page', () => {
+    $( document ).trigger('jqm.init-page');
+    $( document ).unbind('jqm.init-page').bind('jqm.init-page', () => {
       $.mobile.initializePage();
       $('.ui-loader').hide();
     });
