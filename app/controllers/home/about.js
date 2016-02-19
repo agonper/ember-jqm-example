69fdde26d79bddc19ce9bf6b44f452161544aed5
albertogonper <alberto.gonzalez95@gmail.com>
2016-02-20 00:21:02 +0100

    Select with optgroup

--- app/controllers/home/about.js
+++ app/controllers/home/about.js
@@ -3,9 +3,32 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   panelId: "about",
   options: [
-    {value: "first", text: "First"},
-    {value: "second", text: "Second", disabled: true},
-    {value: "third", text: "Third", selected: true}
+    {text: 'Choose...'},
+    {
+      optgroup: "Group 1",
+      elements: [
+        {value: "first", text: "First"},
+        {value: "second", text: "Second", disabled: true},
+        {value: "third", text: "Third", selected: true}
+      ]
+    },
+    {
+      optgroup: "Group 2",
+      elements: [
+        {value: "fourth", text: "Fourth"},
+        {value: "fifth", text: "Fifth", disabled: true},
+        {value: "sixth", text: "Sixth"}
+      ]
+    },
+    {
+      optgroup: "Group 3",
+      elements: [
+        {value: "seventh", text: "Seventh"},
+        {value: "eighth", text: "Eighth", disabled: true},
+        {value: "ninth", text: "Ninth"}
+      ]
+    },
+    {value: 'other', text: "Other (specify)"}
   ],
   onFile: Ember.observer('files', function () {
     var file = this.get('files')[0];
